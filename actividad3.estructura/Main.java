9a72c9a7bdca244b8ec10d75464c589de1ff7034
ktorresagui <al03022345@tecmilenio.mx>
2025-09-04 14:25:20 -0600

    Create Main.java

---
+++ actividad3.estructura/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Clase principal que ejecuta el programa.
+ * Llama al menú de opciones.
+ */
+public class Main {
+
+    /**
+     * Método principal que inicia la ejecución del programa.
+     * @param args argumentos de línea de comandos (no se usan)
+     */
+    public static void main(String[] args) {
+        Menu.mostrarMenu();
+    }
+}
